0f275002b84d88bddac8dafc601f475725e12ce4
ambern115 <nels9242@umn.edu>
2020-11-18 17:54:33 -0600

    Changed index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var http = require('http').createServer(app);
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-express()
-  .get('/', (req, res) => res.render('<h1>Hellow world</h1>'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+//express()
+ // .get('/', (req, res) => res.render('<h1>Hellow world</h1>'))
+  //.listen(PORT, () => console.log(`Listening on ${ PORT }`));
   
-// app.get('/', (req, res) => {
-//   res.send('<h1>Hellow world</h1>');
-// });
+app.get('/', (req, res) => {
+  res.send('<h1>Hellow world</h1>');
+});
 
-// http.listen(3000, () => {
-//   console.log('listening on localhost:3000');
-// });
+http.listen(PORT, () => {
+  console.log('listening on localhost:3000');
+});
