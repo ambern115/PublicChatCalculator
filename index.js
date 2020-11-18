6fc3a6cee33da84f5b2699e7b92ebfafc876df90
ambern115 <nels9242@umn.edu>
2020-11-18 17:45:40 -0600

    Attempt to get online build working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { listenerCount } = require('process');
 var app = require('express')();
 var http = require('http').createServer(app);
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 express()
   .get('/', (req, res) => res.render('<h1>Hellow world</h1>'))
