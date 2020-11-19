f5f43d971b0f7d68ff6f22148b2b0df93aa76015
ambern115 <nels9242@umn.edu>
2020-11-18 18:13:51 -0600

    Added socket.io

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-const path = require('path');
+var io = require('socket.io')(http);
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 http.listen(PORT, () => {
