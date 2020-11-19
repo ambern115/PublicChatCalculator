17bb14635ff04dbf13e117bba1aa62ba4daa0405
ambern115 <nels9242@umn.edu>
2020-11-18 19:57:31 -0600

    Trying to evaluate text and post to chat

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-var app = require('express')();
 var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-const PORT = process.env.PORT || 5000;
 var path = require('path');
+const PORT = process.env.PORT || 5000;
 
+// __dirname is the folder that index.js is in
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Get the path to the `public` folder.
-// __dirname is the folder that `app.js` is in.
+// Get path to the public folder - used to access style sheet
 var publicPath = path.resolve(__dirname, 'public');
-
-// Serve this path with the Express static file middleware.
+// Serve this path with the Express static file middleware
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
