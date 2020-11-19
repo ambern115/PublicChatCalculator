0b2123663b18b31f74a297a5fd84d1dc5133672f
ambern115 <nels9242@umn.edu>
2020-11-18 18:44:25 -0600

    Added path to style.css

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const PORT = process.env.PORT || 5000;
+var path = require('path');
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+// Get the path to the `public` folder.
+// __dirname is the folder that `app.js` is in.
+var publicPath = path.resolve(__dirname, 'style.css');
+
+// Serve this path with the Express static file middleware.
+app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
